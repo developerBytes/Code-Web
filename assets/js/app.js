99f31c609b3ce9ae8d3871fce6007fdc46c94b6c
Sagar <sagar.tomar@qsstechnosoft.com>
2022-08-30 16:27:19 +0530

    Add Scroll Bar

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,3 +22,48 @@ function linkAction() {
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
+/* Scroll Section Active Link */
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+/*Change Background Header*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+  if (this.scrollY >= 80) {
+    header.classList.add("scroll-header");
+  } else {
+    header.classList.remove("scroll-header");
+  }
+}
+window.addEventListener("scroll", scrollHeader);
+
+/*Scroll Top*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  if (this.scrollY >= 560) {
+    scrollUp.classList.add("show-scroll");
+  } else {
+    scrollUpclassList.remove("show-scroll");
+  }
+}
+window.addEventListener("scroll", scrollUp);
